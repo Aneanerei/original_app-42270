4a5e9587586bd1a83cfcf26ec2b47c8daa2cdd95
Aneanerei <tamu.m.thr@gmail.com>
2025-04-30 16:26:31 +0900

    デザイン変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_29_051757) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_30_072535) do
+  create_table "events", charset: "utf8mb3", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
