988e9fe0b295b2f698740500bf8d7714138c6910
Aneanerei <tamu.m.thr@gmail.com>
2025-05-13 14:13:41 +0900

    Update 20250508080305_add_foreign_key_to_work_times.rb

--- db/migrate/20250508080305_add_foreign_key_to_work_times.rb
+++ db/migrate/20250508080305_add_foreign_key_to_work_times.rb
@@ -1,7 +1,5 @@
 class AddForeignKeyToWorkTimes < ActiveRecord::Migration[7.1]
- def change
-    # エラー回避：すでにあるか確認してから追加
-    unless foreign_key_exists?(:work_times, :category_work_times)
-      add_foreign_key :work_times, :category_work_times
-    end
+  def change
+    add_foreign_key :work_times, :category_work_times
+  end
 end
